ee10723b06878cdb425acd994fddec37b4ae5000
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-10-15 23:06:44 +0100

    Initial commit

---
+++ src/test/java/com/afam/cocodeapi/CocodeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afam.cocodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocodeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
